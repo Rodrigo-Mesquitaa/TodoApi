31aff7f5c4589805287dcb88f290c64848f2cd44
Rodrigo Mesquita <rodrigo09.mesquita@gmail.com>
2020-06-02 21:01:59 -0300

    Primeira API Core

---
+++ TodoApi/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions<DataContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
